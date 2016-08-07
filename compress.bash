1b7ae4e32fe6da7aaf556856c6e44e7387d32404
topet2k12001 <topet2k12001@users.noreply.github.com>
2016-08-07 20:15:46 +0800

    For AOSP-G3 Repositories (AOSP with CAF)
    Reference thread in XDA: http://forum.xda-developers.com/lg-g3/development/rom-aosp-code-aurora-forum-t3400800

--- compress.bash
+++ compress.bash
@@ -1,6 +1,6 @@
-name=AICP
+name=AOSP-G3
  #https:// is mandatory in manifest link!
-manifest=https://github.com/AICP/platform_manifest.git
+manifest=https://github.com/AOSP-G3/platform_manifest.git
 branch=mm6.0
 
 ./skadoo.sh $name $manifest $branch
