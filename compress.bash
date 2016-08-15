caeb6380043db67a8e3799ae0a8c6e25923bd5e4
Qiangong2 <qiangong2@126.com>
2016-08-15 11:58:10 -0700

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -1,7 +1,7 @@
-name=Omni
+name=LuneOS
  #https:// is mandatory in manifest link!
-manifest=https://github.com/omnirom/android.git
-branch=android-4.4
+manifest=https://github.com/webOS-ports/android.git
+branch=wop-11.0
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
 export compressrepo=true
