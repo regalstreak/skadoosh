d7cab74036f9926077a47c106c0332558b79f731
Qiangong2 <qiangong2@126.com>
2016-08-17 11:05:11 -0700

    Add AOSB

--- compress.bash
+++ compress.bash
@@ -1,7 +1,7 @@
-name=FlareROM
+name=AOSB
  #https:// is mandatory in manifest link!
-manifest=https://github.com/FlareROM/android.git
-branch=1.0-MM
+manifest=https://github.com/AOSB/android.git
+branch=kitkat
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
 export compressrepo=true
