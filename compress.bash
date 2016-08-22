fdef7e9e09218a0ed3dc785afafecea8888e03ab
Arizaini <famegalaxy614@gmail.com>
2016-08-23 01:45:07 +0700

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -1,6 +1,6 @@
 name=AOSPA-legacy
  #https:// is mandatory in manifest link!
-manifest=https://github.com/AOSPA-legacy/manifest.git -b kitkat
+manifest=https://github.com/AOSPA-legacy/manifest.git
 branch=kitkat
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
