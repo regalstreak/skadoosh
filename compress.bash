2a1c3c1897ea30e7ac6bd01f8d91216dbfcb1d2d
Arizaini <famegalaxy614@gmail.com>
2016-08-23 01:52:16 +0700

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -4,8 +4,8 @@ manifest=https://github.com/AOSPA-legacy/manifest.git
 branch=kitkat
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
-export compressrepo=true
-export compressnorepo=false
+export compressrepo=false
+export compressnorepo=true
 
 ./skadoo.sh $name $manifest $branch
 
