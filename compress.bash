338514ef4471ff8fa835d5f503503f9e99194d1b
Akshay Menon <24o81994@gmail.com>
2016-08-29 23:23:13 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -1,7 +1,7 @@
 name=FlareROM
  #https:// is mandatory in manifest link!
-manifest=https://github.com/FlareROM/android.git
-branch=1.0-MM
+manifest=https://android.googlesource.com/platform/manifest
+branch=android-6.0.1_r63
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
 export compressrepo=true
