491a08c72238540797bd744529f3d149671c47c7
mohammad.afaneh <mohammad.afaneh92@gmail.com>
2016-09-02 13:32:10 +0300

    AOSP 7.0.0_r1 no repo

--- compress.bash
+++ compress.bash
@@ -1,10 +1,10 @@
 name=FlareROM
  #https:// is mandatory in manifest link!
-manifest=https://github.com/FlareROM/android.git
-branch=1.0-MM
+manifest=https://android.googlesource.com/platform/manifest
+branch=android-7.0.0_r1
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
-export compressrepo=true
+export compressrepo=false
 export compressnorepo=true
 
 ./skadoo.sh $name $manifest $branch
