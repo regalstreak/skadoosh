ec628a384c4a78bc918c821dc5449ec1da185393
mohammad.afaneh <mohammad.afaneh92@gmail.com>
2016-09-02 13:27:25 +0300

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -5,7 +5,7 @@ manifest=https://github.com/FlareROM/android.git
 branch=android-7.0.0_r1
 
 ## comment any of the self explanatory lines if you are only interested in a particular type, default is both.
-export compressrepo=true
+export compressrepo=false
 export compressnorepo=true
 
 ./skadoo.sh $name $manifest $branch
