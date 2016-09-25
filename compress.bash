b4e6721390959440bdd7eef01a786e90cc667236
GNVM <gnathvm@gmail.com>
2016-09-25 09:02:31 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -11,19 +11,19 @@
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
 
-name=ColorOS
+name=Nitrogen OS
 
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
 
-manifest=https://github.com/Qiangong2/patchrom/default.xml
+manifest=https://github.com/nitrogen-project/android_manifest.git -b n
 
 
 # Manifest branch.
 # Example: cm-14.0
 
-branch=jellybean42
+branch=n
 
 
 ### Compression Configuration ###
@@ -39,7 +39,7 @@ export compressrepo=true
 
 # compressnorepo
 # If true, will compress the stuff except for the .repo folder
-export compressnorepo=true
+export compressnorepo=false
 
 
 ### Finally, execute the stuff. ###
