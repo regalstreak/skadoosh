0b0ef625937d07872c4aa15ddb0350611b0622a0
luki122 <super_robot6@o2.pl>
2016-10-23 20:19:51 +0200

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -11,19 +11,19 @@
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
 
-name=PureNexusProject
+name=AOSP
 
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
 
-manifest=https://github.com/PureNexusProject/manifest
+manifest=https://android.googlesource.com/platform/manifest
 
 
 # Manifest branch.
 # Example: cm-14.0
 
-branch=n
+branch=android-4.2.2_r1
 
 
 ### Compression Configuration ###
