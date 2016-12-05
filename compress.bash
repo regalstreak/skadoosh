a4d36450cc5042e967ce53cdab77e79baa2b4faa
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-05 14:31:57 +0530

    Implement shallow and deep clones
    Better replacement for repo and non-repo archives since now even smaller archives can take upstream updates

--- compress.bash
+++ compress.bash
@@ -2,7 +2,7 @@
 
 # Authors - Neil "regalstreak" Agarwal, Harsh "MSF Jarvis" Shandilya, Tarang "DigiGoon" Kagathara
 # 2016
-# This file is used to run skadoo.sh easily. 
+# This file is used to run skadoo.sh easily.
 # Can also be configured with a webhook along with automation as regalstreak and msf-jarvis have done.
 
 
@@ -25,23 +25,5 @@ manifest=https://github.com/PureNexusProject/manifest
 
 branch=n
 
-
-### Compression Configuration ###
-
-# Change the value only if you know what these are.
-# Default is true for both.
-
-# compressrepo
-# If true, will compress the .repo folder.
-
-export compressrepo=true
-
-
-# compressnorepo
-# If true, will compress the stuff except for the .repo folder
-export compressnorepo=true
-
-
-### Finally, execute the stuff. ###
 
 /bin/bash skadoo.sh $name $manifest $branch
