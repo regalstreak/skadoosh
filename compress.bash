3d6147b47ebcf2e05c6b25d0378a1068995ef75a
vishalsharma9001 <vishalsharma9001@gmail.com>
2016-12-07 00:43:28 +0530

    AOKP MM (#82)

--- compress.bash
+++ compress.bash
@@ -11,19 +11,13 @@
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
 
-name=ResurrectionRemix
-
-
+name=AOKP
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
 
-manifest=https://github.com/ResurrectionRemix/platform_manifest
-
-
+manifest=https://github.com/AOKP/platform_manifest
 # Manifest branch.
 # Example: cm-14.0
 
-branch=nougat
-
-
+branch=mm
 /bin/bash skadoo.sh $name $manifest $branch
