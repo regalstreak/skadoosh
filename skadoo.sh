fbbfc23417704ab1f27116811001a6740afee52e
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-14 13:11:50 +0530

    Address bugs

--- skadoo.sh
+++ skadoo.sh
@@ -10,32 +10,31 @@ DIR=$(pwd)
 LINK=$2
 ROMNAME=$1
 
-
 # Functions
 installstuff(){
     # VENDOREDIT
     if [ "$HUTIYA" != "nope" ]; then
 
         # Check if repo is installed
-        if [ !$( which repo ) ]; then
+        if [ $( which repo ) == "" ]; then
           echo "Installing repo for Downloading the sources"
           sudo apt install repo
         fi
 
         # Check if user has bc, if not install it
-        if [ !$( which bc ) ]; then
+        if [ $( which bc ) == "" ]; then
           echo "Installing bc"
           sudo apt install bc
         fi
 
         # Check if user has pxz, if not install it
-        if [ !$( which pxz ) ]; then
+        if [ $( which pxz ) == "" ]; then
           echo "Installing pxz for multi-threaded compression"
           sudo apt install pxz
         fi
 
         # Check if user has wput, if not install it
-        if [ !$( which wput ) ]; then
+        if [ $( which wput )  == "" ]; then
           echo "Installing wput for uploading"
           sudo apt install wput
         fi
@@ -68,19 +67,9 @@ checkfinishtime(){
 doshallow(){
     cd $DIR; mkdir -p $ROMNAME/shallow; cd $ROMNAME/shallow
 
-    repo init -u $LINK -b $BRANCH --depth 1 --reference $DIR/$ROMNAME/full/
+    repo init -u $LINK -b $BRANCH --depth 1 -q --reference $DIR/$ROMNAME/full/
 
     THREAD_COUNT_SYNC=49
-
-    # VENDOREDIT
-    if [ $(hostname) != 'krieger' ] || [ "$HUTIYA" != "nope" ]; then
-
-        # Gather the number of threads
-        CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
-        # Use 8 times the cpucount
-        THREAD_COUNT_SYNC=$(($CPU_COUNT * 8))
-
-    fi
 
     # Sync it up!
     time repo sync -c -f --force-sync --no-clone-bundle --no-tags -j$THREAD_COUNT_SYNC
@@ -89,7 +78,7 @@ doshallow(){
     cd $DIR/$ROMNAME/
 
     mkdir $ROMNAME-$BRANCH-shallow-$(date +%Y%m%d)
-    mv shallow/.repo $ROMNAME-$BRANCH-shallow-$(date +%Y%m%d)
+    mv shallow/.repo/ $ROMNAME-$BRANCH-shallow-$(date +%Y%m%d)
     cd $DIR/$ROMNAME/
     export XZ_OPT=-9e
     time tar -I pxz -cvf $ROMNAME-$BRANCH-shallow-$(date +%Y%m%d).tar.xz $ROMNAME-$BRANCH-shallow-$(date +%Y%m%d)/
@@ -133,24 +122,14 @@ dofull(){
 
     THREAD_COUNT_SYNC=49
 
-    # VENDOREDIT
-    if [ $(hostname) != 'krieger' ] || [ "$HUTIYA" != "nope" ]; then
-
-        # Gather the number of threads
-        CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
-        # Use 8 times the cpucount
-        THREAD_COUNT_SYNC=$(($CPU_COUNT * 8))
-
-    fi
-
     # Sync it up!
-    time repo sync -c -f --force-sync --no-clone-bundle --no-tags -j$THREAD_COUNT_SYNC
+    time repo sync -c -f --force-sync -q --no-clone-bundle --no-tags -j$THREAD_COUNT_SYNC
 
 
     cd $DIR/$ROMNAME/
 
     mkdir $ROMNAME-$BRANCH-full-$(date +%Y%m%d)
-    mv shallow/.repo $ROMNAME-$BRANCH-full-$(date +%Y%m%d)
+    mv full/.repo $ROMNAME-$BRANCH-full-$(date +%Y%m%d)
     cd $DIR/$ROMNAME/
     export XZ_OPT=-9e
     time tar -I pxz -cvf $ROMNAME-$BRANCH-full-$(date +%Y%m%d).tar.xz $ROMNAME-$BRANCH-full-$(date +%Y%m%d)/
@@ -180,14 +159,22 @@ dofull(){
 
     cd $DIR/$ROMNAME/
 
-    if [ -e $FULL ]; then
-    wput $FULL ftp://"$USER":"$PASSWD"@"$HOST"/
-    else
-    echo "$FULL does not exist. Not uploading the shallow tarball."
-    fi
 }
 
+upload(){
+  if [ -e $FULL ]; then
+    wput $FULL ftp://"$USER":"$PASSWD"@"$HOST"/
+  else
+    echo "$FULL does not exist. Not uploading the shallow tarball."
+  fi
 
+  if [ -e $SHALLOW ]; then
+  wput $SHALLOW ftp://"$USER":"$PASSWD"@"$HOST"/
+  else
+  echo "$SHALLOW does not exist. Not uploading the shallow tarball."
+  fi
+
+}
 # Do All The Stuff
 
 doallstuff(){
@@ -214,5 +201,6 @@ if [ $? -eq 0 ]; then
   rm -rf $DIR/$ROMNAME
 else
   echo "Something failed :(";
+  rm -rf $DIR/$ROMNAME/shallow $DIR/$ROMNAME/full
   exit 1;
 fi
