ccd16a3a00f590935c8c53ff0ed0082578ce4997
TheHercules <abhijio082@gmail.com>
2016-12-14 11:18:17 +0530

    RR update compress.bash (#70)
    * Update compress.bash
    
    * Update compress.bash

--- compress.bash
+++ compress.bash
@@ -11,13 +11,21 @@
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
 
-name=AOKP
+
+name=ResurrectionRemix
+
+
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
 
-manifest=https://github.com/AOKP/platform_manifest
+manifest=https://github.com/ResurrectionRemix/platform_manifest
+
 # Manifest branch.
 # Example: cm-14.0
 
-branch=mm
+branch=marshmallow
+
+
+### Finally, execute the stuff. ###
+
 /bin/bash skadoo.sh $name $manifest $branch
