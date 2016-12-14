25193440b3a4d5b3d6fe33178d6a4a53d02a5ba1
NazarMohamed <nazxda@gmail.com>
2016-12-14 13:34:43 +0530

    SlimRoms Nougat (#75)

--- compress.bash
+++ compress.bash
@@ -11,18 +11,18 @@
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
 
-name=MIUI8Marshmallow
+name=SlimRoms
 
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
 
-manifest=https://github.com/MiCode/patchrom
+manifest=https://github.com/SlimRoms/platform_manifest
 
 # Manifest branch.
 # Example: cm-14.0
 
-branch=marshmallow
+branch=ng7.0
 
 ### Finally, execute the stuff. ###
 
