cc3057b4693638844b1ef7a0bccc767a48f094de
hima013 <himavanth93@gmail.com>
2017-07-09 14:14:32 +0530

    FlymeOS Marshmallow (#117)

--- compress.bash
+++ compress.bash
@@ -5,23 +5,19 @@
 # This file is used to run skadoo.sh easily.
 # Can also be configured with a webhook along with automation as regalstreak and msf-jarvis have done.
 
-
 ### Manifest Configuration ###
 
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=AOSPExtended
-
+name=FlymeOS
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/AospExtended/manifest.git
-
+manifest=https://github.com/FlymeOS/manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=7.x
-
+branch=marshmallow-6.0
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
