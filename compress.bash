cc5b210cc6daf74e49f2bf7047c83b5073c34dfc
Growtopia Jaw <growtopiajaw@gmail.com>
2017-07-11 09:23:20 +1200

    ResurrectionRemix optimised-lp5.1 (#128)

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=DirtyUnicorns
+name=ResurrectionRemix
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/DirtyUnicorns/android_manifest
+manifest=https://github.com/ResurrectionRemix/platform_manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=n7x
+branch=optimized-lollipop5.1
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
