47332a3658a58d36933614fa5e4512622e228e53
Vinesh Iyappan <Hari-08@users.noreply.github.com>
2017-07-10 23:50:13 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=GZR-ValidusOS
+name=halogenOS
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/GZR-ValidusOS/manifest
+manifest=https://github.com/halogenOS/android_manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=n7.1.2
+branch=XOS-7.1
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
