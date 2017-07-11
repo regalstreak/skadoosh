c6301b92c814ec34e74933d2bdc208c2678a8052
georgemath24 <georgemannil2000@gmail.com>
2017-07-11 19:02:11 +0530

    Atomic-os

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=Candy
+name=Atomic-os
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/CandyRoms/candy
+manifest=https://github.com/Atomic-OS/platform_manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=c7.1.2
+branch=n-mr2
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
