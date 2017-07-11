0ac81fe6373bcfa1a1cbb54f540d05d0ae5e7363
Vinesh Iyappan <Hari-08@users.noreply.github.com>
2017-07-11 16:31:25 +0530

    halogenOS 7.1 (#130)

--- compress.bash
+++ compress.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Authors - Neil "regalstreak" Agarwal, Harsh "MSF Jarvis" Shandilya, Tarang "DigiGoon" Kagathara
-# 2016
+# 2016, 2017
 # This file is used to run skadoo.sh easily.
 # Can also be configured with a webhook along with automation as regalstreak and msf-jarvis have done.
 
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=Miui
+name=halogenOS
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/MiCode/patchrom
+manifest=https://github.com/halogenOS/android_manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=marshmallow
+branch=XOS-7.1
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
