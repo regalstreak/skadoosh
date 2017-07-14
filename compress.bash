3512fd5059647487fe29e2419e70574d4f15872d
AbdelfattahToulaoui <aft.power.2014@gmail.com>
2017-07-14 23:39:55 +0100

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=OctOS
+name=ParanoidAndroid
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/Team-OctOS/platform_manifest.git
+manifest=https://github.com/AOSPA/manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=oct-mm
+branch=nougat-mr2
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
