2eaf9fe8b45a46eea4ae2163922b73c13c5fc0fe
Faradis <Druboo666@users.noreply.github.com>
2017-07-19 00:56:53 +0600

    TeslaRom-N

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=ResurrectionRemix
+name=TeslaRom-N
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/ResurrectionRemix/platform_manifest
+manifest=https://github.com/TeslaRom-N/manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=nougat
+branch=n7.1.2
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
