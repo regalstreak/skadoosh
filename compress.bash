84a1babda4b66fa5b2fe44f820155cf9703e4f97
Karanvir Singh <karanvir.singh61@yahoo.in>
2017-08-14 23:47:59 +0530

    Add MoKee ROM sources

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=HexagonROM
+name=MoKeeRom
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/HexagonRom/android_platform_manifest
+manifest=https://github.com/MoKee/android
 
 # Manifest branch.
 # Example: cm-14.0
-branch=hex-7.1
+branch=mkn-mr1
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
