02abf23adabf15ab31462cfade7939e9b5e7aae5
SlimShadys <gianmarcoscarano@gmail.com>
2017-08-18 18:10:36 +0200

    Support AOSP 7.1.2 (#152)

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=HexagonROM
+name=AOSP
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/HexagonRom/android_platform_manifest
+manifest=https://android.googlesource.com/platform/manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=hex-7.1
+branch=android-7.1.2_r27
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
