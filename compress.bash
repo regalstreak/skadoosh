2b1b0b744252d4a90f1621d4d32fe1b103dec31c
Aman Kumar <firelord.xda@lineageos.org>
2017-11-12 02:31:15 +0530

    PornAOSP Oreo

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=DND
+name=PornAOSP
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/DarkNess-reDefined/manifest
+manifest=https://github.com/PornAOSP/platform_manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=n7x
+branch=o
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
