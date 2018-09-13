e73dbac2bb01cdc95150035ab3c99824d4ee797a
Naveen Singh <36371707+Naveen3Singh@users.noreply.github.com>
2018-09-13 10:13:38 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=DND
+name=AOSP
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/DarkNess-reDefined/manifest
+manifest=https://android.googlesource.com/platform/manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=n7x
+branch=android-5.1.1_r38
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
