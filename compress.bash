ab549afd5a5d0911ce90c5d67424b26c916c2138
akshatGupta2 <36946364+akshatGupta2@users.noreply.github.com>
2019-02-06 16:43:21 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -1,22 +1,18 @@
-#!/bin/bash
-
-# Authors - Neil "regalstreak" Agarwal, Harsh "MSF Jarvis" Shandilya, Tarang "DigiGoon" Kagathara
-# 2016, 2017
-# This file is used to run skadoo.sh easily.
-# Can also be configured with a webhook along with automation as regalstreak and msf-jarvis have done.
-
-### Manifest Configuration ###
+#!### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
+
 name=AOSP
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
+
 manifest=https://android.googlesource.com/platform/manifest
+
 # Manifest branch.
 # Example: cm-14.0
 
-branch=android-9.0.0
+branch=android-9.0.0_r11
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
